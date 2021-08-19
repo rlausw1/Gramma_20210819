00b56bd112e0222b204220a1df5a7654ef04875d
김현지 <guswlhjk@naver.com>
2021-08-19 10:51:52 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/gramma_20210819/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nepplus.gramma_20210819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //코틀린 문법 연습용 프로젝트 (UI작업 X)
+    }
+}
